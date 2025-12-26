b543940b6e882fdda866c8496e878d4ef02de10a
gdan <1173882990@qq.com>
2025-12-26 14:31:20 +0700

    健康检查

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,24 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'health-check',
+      configureServer(server) {
+        server.middlewares.use((req, res, next) => {
+          if (req.url === '/api/health') {
+            res.setHeader('Content-Type', 'application/text');
+            res.setHeader('Cache-Control', 'no-cache');
+            res.statusCode = 200;
+            res.end("ok");
+          } else {
+            next();
+          }
+        });
+      },
+    },
+  ],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
